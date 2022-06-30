b876e21df23346dfe1f036f4c4e5c5e1b2ff3f49
IrdiZ <67896863+IrdiZ@users.noreply.github.com>
2022-06-30 21:59:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'Which is a programming lang?',
+        a: 'Assembly',
+        b: 'lot',
+        c: 'tol',
+        d: 'rofl',
+        correct: 'a' 
+    }, 
+    {
+        question: 'US President',
+        a: 'Bush',
+        b: 'Obama',
+        c: 'Trump',
+        d: 'Biden',
+        correct: 'd'
+    },
+    {
+        question: 'Most popular prog lang',
+        a: 'Javascript',
+        b: 'Python',
+        c: 'Java',
+        d: 'c++',
+        correct: 'a'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Hyperturn MoonLanding',
+        c: 'High Turing Machine Localisation',
+        d: 'HElicopters Terminals Motorboats LAmborginis',
+        correct: 'a'
+    },
+    {
+        question: 'What year was JS launched?',
+        a: '2020',
+        b: '1995',
+        c: '1996',
+        d: '1994',
+        correct: 'b'
+    }
+] 
